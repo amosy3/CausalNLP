e06c0062b330d4bbc6d4a50807eea5f3661f2efa
amosy3 <ohad.amosy@mail.huji.ac.il>
2024-10-08 12:22:45 +0300

    checkpoint

--- CausalNLP/appprox.py
+++ CausalNLP/appprox.py
@@ -5,11 +5,11 @@ from torch import nn
 import argparse
 from utils import Logger
 from backbones import LanguageModel
-
+from itertools import product
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--backbone",type=str, default='gpt2')
+    parser.add_argument("--backbone",type=str, choices=['gpt2', 'deberta'], default='gpt2')
     parser.add_argument("--method",type=str, choices=['approx', 'train_model'], default='approx')
     parser.add_argument("--log_file", type=str, default='')
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
 
     lm = LanguageModel()
-    df = pd.read_csv('../datasets/cv_w_cf.csv')
+    # df = pd.read_csv('../datasets/cv_w_cf.csv')
 
     if args.method == "train_model":
         # Create train and test loader
@@ -57,8 +57,37 @@ if __name__ == "__main__":
         # Evaluate the model
         trainer.evaluate()
 
+    if args.method == "approx":
+        # load model weights
+        # Eval ICaCE
+        concepts = ['Gender', 'Education', 'Socioeconomic_Status', 'Age_group', 'Certificates', 'Volunteering', 'Race',
+                    'Work_Experience_group']
+        df = pd.read_csv('../datasets/cv_w_cf.csv')
+        df_org = df[df['CF_on'] == 'ORG']
+        for c in concepts:
+            print("Concept = %s" % c)
+            posible_values = df_org[c].unique()
+            posible_cf = [p for p in product(posible_values, repeat=2)]
+            for (cf, ct) in posible_cf:
+                if cf == ct:
+                    continue
+                print(cf, ct)
+                mask = df_org[c] == cf
+                x = df_org[mask].sample()
+                keep_concepts = [z for z in concepts if z != c]
+                change_concepts = ['Candidate_id', c]
+                matching_rows = df_org[(df_org[keep_concepts] == x[keep_concepts].values).all(axis=1)]
+                valid_cf = matching_rows[
+                    (matching_rows[change_concepts] != matching_rows[change_concepts].values).all(axis=1)]
+                if valid_cf.shape[0] == 0.0:
+                    print('No counterfactuals for %s->%s' % (cf, ct))
+                else:
+                    x_cf = valid_cf.sample()
+                    # calc the diff
 
-    # Testing
+            # for i, row in df_org.iterrows():
+
+        # Testing
     X = df["CV_statement"][:1].tolist()
     y = df["Good_Employee"][:1].tolist()
     features = lm.extract_features(X)
