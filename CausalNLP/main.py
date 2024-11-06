a7412085b8cb95c8d4fbfb56787e0b4ff28c4a9c
Ohad Amosy <amosy3@nlp12.iem.technion.ac.il>
2024-11-06 14:52:29 +0200

    apporx works without real CF due to small dataset

--- CausalNLP/main.py
+++ CausalNLP/main.py
@@ -63,38 +63,74 @@ if __name__ == "__main__":
 
         # Evaluate the model
         trainer.evaluate()
+        exit()
+
+    # load model weights
+    model, tokenizer = get_model(args.backbone, ckpt='../pretrain_models/%s' % args.backbone)
+    model.eval()
+
+    def get_logits_from_text(model, tokenizer, sentences):
+        inputs = tokenizer(sentences, return_tensors='pt', padding=True, truncation=True)
+        # Get input_ids and attention mask
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask=attention_mask)
+        return outputs.logits
+
+    def get_cf_sample(concepts, current_concept, full_df, org_smaple):
+                        keep_concepts = [z for z in concepts if z != current_concept]
+                        diff_concepts = [current_concept] #['Candidate_id', c]
+                        matching_rows = full_df[(full_df[keep_concepts] == org_smaple[keep_concepts].values).all(axis=1)]
+                        valid_cf = matching_rows[(matching_rows[diff_concepts] != org_smaple[diff_concepts].values).all(axis=1)]
+                        if valid_cf.shape[0] == 0.0:
+                            print('No counterfactuals for %s->%s !!!!' % (org_concept, do_concept))
+                            return full_df.sample()
+                        else:
+                            return valid_cf.sample()
 
     if args.method == "approx":
-        # load model weights
-        model, tokenizer = get_model(args.backbone, ckpt='../pretrain_models/%s' % args.backbone)
-        print("Done!")
-        exit()
+        metrics = {}    
         # Eval ICaCE
         concepts = ['Gender', 'Education', 'Socioeconomic_Status', 'Age_group', 'Certificates', 'Volunteering', 'Race', 'Work_Experience_group']
+        text = 'CV_statement'
         df = pd.read_csv('../datasets/cv_w_cf.csv')
         df_org = df[df['CF_on'] == 'ORG']
-        for c in concepts:
-            print("Concept = %s" % c)
-            posible_values = df_org[c].unique()
+        
+        for current_concept in concepts:
+            print("Concept = %s" % current_concept)
+            posible_values = df_org[current_concept].unique()
             posible_cf = [p for p in product(posible_values, repeat=2)]
-            for (cf, ct) in posible_cf:
-                if cf == ct:
+            for (org_concept, do_concept) in posible_cf:
+                if org_concept == do_concept:
                     continue
-                print(cf, ct)
-                mask = df_org[c] == cf
-                x = df_org[mask].sample()
-                keep_concepts = [z for z in concepts if z != c]
-                change_concepts = ['Candidate_id', c]
-                matching_rows = df_org[(df_org[keep_concepts] == x[keep_concepts].values).all(axis=1)]
-                valid_cf = matching_rows[
-                    (matching_rows[change_concepts] != matching_rows[change_concepts].values).all(axis=1)]
-                if valid_cf.shape[0] == 0.0:
-                    print('No counterfactuals for %s->%s' % (cf, ct))
-                else:
-                    x_cf = valid_cf.sample()
-                    # calc the diff
 
-            # for i, row in df_org.iterrows():
+                
+                print("Estimating the effect of changing %s to %s" % (org_concept, do_concept))
+                metrics[(current_concept, org_concept, do_concept)] = {'l2':[], 'cosine':[], 'norm_diff':[]}
+
+                mask_org = df_org[current_concept] == org_concept
+                cdf = df_org[mask_org]
+                for i in range(3):
+                    x_org = cdf.sample()
+                    preds = get_logits_from_text(model, tokenizer, x_org[text].tolist())
+
+                    x_cf = get_cf_sample(concepts, current_concept, cdf, x_org)
+                    cf_preds = get_logits_from_text(model, tokenizer, x_cf[text].tolist())
+
+                    l2 = torch.norm(preds - cf_preds, p=2)
+                    metrics[(current_concept, org_concept, do_concept)]['l2'].append(l2.item())
+
+                    cosine_similarity = torch.nn.functional.cosine_similarity(preds, cf_preds)
+                    metrics[(current_concept, org_concept, do_concept)]['cosine'].append(1 - cosine_similarity.item())
+                    
+                    norm_diff = abs(torch.norm(preds) - torch.norm(cf_preds))
+                    metrics[(current_concept ,org_concept, do_concept)]['norm_diff'].append(norm_diff.item())
+                    
+        logger.log_object(metrics)
+                        
+                    
 
 
 
